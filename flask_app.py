d08b4b0b00e62dee599b034fb81a89be10f77699
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2025-04-03 00:50:19 +0300

    Added GET

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,5 @@
+
+
 from flask import *
 import pymysql
 from flask_cors import CORS
@@ -18,9 +20,12 @@ def addArticle():
             date = data.get('date')
             featuredImage = data.get('featuredImage')
             views = data.get('views')
-            commentEnabled = data.get('commentEnabled')
+            comment = data.get('commentEnabled')
             readTime = data.get('readTime')
-
+            if comment==1:
+                commentEnabled="True"
+            else:
+                commentEnabled="False"
             sql="INSERT INTO blog(title,author,content,date,featuredImage,views,commentEnabled,readTime) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
             cursor = connection.cursor()
             cursor.execute(sql,(title,author,content,date,featuredImage,views,commentEnabled,readTime))
@@ -28,9 +33,25 @@ def addArticle():
             return jsonify({'message':'Article added successfuly'}),200
 
         except Exception as e:
-            return jsonify({'error':f"There was an error {e}"}),500
+            return jsonify({'error':f"There was an error {str(e)}"}),500
 
     return jsonify({'message':'Added sucessully'})
+
+@app.route('/Api/getArticles',methods=['POST','GET'])
+def getArticles():
+    if request.method=='GET':
+        try:
+            sql="SELECT * FROM blog"
+            cursor=connection.cursor()
+            cursor.execute(sql)
+            if cursor.rowcount==0:
+                return jsonify({'message':'No Articles Available'}),404
+            else:
+                article=cursor.fetchall()
+                return jsonify({'message':article}),200
+        except Exception as e:
+            return  jsonify({'message':'Server error'}),500
+    return jsonify({'message':'No Requests made'})
 @app.route('/Api/deleteArticles/<id>',methods=['POST','GET'])
 def deleteArticles(id):
     if request.method=='post':
@@ -46,7 +67,7 @@ def deleteArticles(id):
                 return jsonify({'error':'The Article Was Nof Found'},404)
             else:
                 try:
-                    deleteArticle = "DELETE * FROM blog WHERE id=%s"
+                    deleteArticle = "DELETE FROM blog WHERE id=%s"
                     cursor=connection.cursor()
                     cursor.execute(deleteArticle,(id))
                     return jsonify({'message':'Article Deleted'},200)
