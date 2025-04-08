d858df3d01b905600a6f99e6408d6fd78b821dc5
Michael-star26 <130248327+Michael-star26@users.noreply.github.com>
2025-04-08 14:12:03 +0300

    added images

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,22 @@
 
-
+import pymysql.cursors
 from flask import *
 import pymysql
 from flask_cors import CORS
-# connection=pymysql.connect(user='Vertexlabs',database='Vertexlabs$default',host='Vertexlabs.mysql.pythonanywhere-services.com',password='michael1234')
+from datetime import datetime
+import requests
+# connection=pymysql.connect(user='root',database='vertexlabs',host='localhost')
 connection=pymysql.connect(user='sql7770379',database='sql7770379',host='sql7.freesqldatabase.com',password='zsJSBacLzW')
 app=Flask(__name__)
 CORS(app)
 @app.route('/Api/addArticle',methods=['POST','GET'])
 def addArticle():
+    # try:
+    #     connection = pymysql.connect(user='sql7770379', database='sql7770379', host='sql7.freesqldatabase.com',password='zsJSBacLzW')
+    # except Exception as e:
+    #     return jsonify({'message':'Failed to connect to DB'}
+    now=datetime.now()
+    today=now.strftime("%B %d, %Y")
     if request.method=='POST':
         try:
             data=request.get_json()
@@ -17,7 +25,7 @@ def addArticle():
             title = data.get('title')
             author = data.get('author')
             content = data.get('content')
-            date = data.get('date')
+            date = today
             featuredImage = data.get('featuredImage')
             views = data.get('views')
             comment = data.get('commentEnabled')
@@ -35,47 +43,71 @@ def addArticle():
         except Exception as e:
             return jsonify({'error':f"There was an error {str(e)}"}),500
 
-    return jsonify({'message':'Added sucessully'})
-
-@app.route('/Api/getArticles',methods=['POST','GET'])
-def getArticles():
-    if request.method=='GET':
-        try:
-            sql="SELECT * FROM blog"
-            cursor=connection.cursor()
-            cursor.execute(sql)
-            if cursor.rowcount==0:
-                return jsonify({'message':'No Articles Available'}),404
-            else:
-                article=cursor.fetchall()
-                return jsonify({'message':article}),200
-        except Exception as e:
-            return  jsonify({'message':'Server error'}),500
     return jsonify({'message':'No Requests made'})
+
+@app.route('/Api/getArticles',methods=['GET','POST'])
+def getArticles():
+    try:
+        sql = "SELECT * FROM blog"
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+        article = cursor.fetchall()
+        return jsonify(article), 200
+    except Exception as e:
+        return jsonify({'message': f"Server error {str(e)}"}), 500
+
+    # return jsonify({'message':'No requests found'})
+
+@app.route('/Api/getImages')
+def images():
+    try:
+        category = request.args.get('category', 'catgirl')
+        res=requests.get(f'https://api.nekosia.cat/api/v1/images/catgirl')
+        if res.status_code==200:
+            data=res.json()
+            if data['success']:
+                url = {
+                    'imgUrl': data['image']['original']['url']
+                }
+                return jsonify(url),200
+            else:
+                return jsonify({'message':'no image found'}),404
+        else:
+            return jsonify({'message':'Not found'}),404
+    except Exception as e:
+        return jsonify({'message':'Forbidden'}),500
 @app.route('/Api/deleteArticles/<id>',methods=['POST','GET'])
 def deleteArticles(id):
-    if request.method=='post':
-        try:
-            data = request.get_json()
-            if not(data):
-                return jsonify({'error':'Error, Supply Article ID'},404)
-            id = data.get('id')
-            sql="SELECT * FROM blog WHERE id=%s"
-            cursor=connection.cursor()
-            cursor.execute(sql,(id))
-            if cursor.rowcount==0:
-                return jsonify({'error':'The Article Was Nof Found'},404)
-            else:
-                try:
-                    deleteArticle = "DELETE FROM blog WHERE id=%s"
-                    cursor=connection.cursor()
-                    cursor.execute(deleteArticle,(id))
-                    return jsonify({'message':'Article Deleted'},200)
-                except Exception as e:
-                    return jsonify({'error':f"There was an error {e}"},500)
-
-        except Exception as e:
-            return jsonify({'error':'Server Error'},500)
-    return jsonify({'message':'deleted successfully'})
+    # if request.method=='GET':
+        # try:
+            # data = request.get_json()
+            # if not(data):
+            #     return jsonify({'error':'Error, Supply Article ID'},404)
+            # id = data.get('id')
+        #     sql="SELECT * FROM blog WHERE id=%s"
+        #     cursor=connection.cursor()
+        #     cursor.execute(sql,(id))
+        #     if cursor.rowcount==0:
+        #         return jsonify({'error':'The Article Was Nof Found'},404)
+        #     else:
+        #         try:
+        #             deleteArticle = "DELETE FROM blog WHERE id>%s"
+        #             cursor=connection.cursor()
+        #             cursor.execute(deleteArticle,(id))
+        #             connection.commit()
+        #             return jsonify({'message':'Article Deleted'},200)
+        #         except Exception as e:
+        #             return jsonify({'error':f"There was an error {e}"},500)
+        #
+        # except Exception as e:
+        #     return jsonify({'error':f"Server Error {str(e)}"},500)
+    try:
+        deleteArticle = "DELETE FROM blog"
+        cursor = connection.cursor()
+        cursor.execute(deleteArticle)
+        connection.commit()
+        return jsonify({'message': 'deleted successfully'})
+    except Exception as e:
+        return jsonify({'message': 'Failed terribly'})
 if __name__=="__main__":
     app.run(debug=True)
